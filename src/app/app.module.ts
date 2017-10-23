1235871b3129a0855130742e1af8da0c035a2c41
Győző Horváth <gyozo.horvath@inf.elte.hu>
2017-10-23 16:19:25 +0200

    experiencing with component frameworks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { RouterModule }   from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from "@angular/flex-layout";
+
+import { MatToolbarModule, MatButtonModule, MatMenuModule, MatIconModule,
+      MatFormFieldModule, MatInputModule
+} from "@angular/material";
 
 import { AppComponent } from './app.component';
 import { FruitListComponent } from './fruit-list/fruit-list.component';
@@ -21,7 +26,15 @@ import { RoutingModule } from "./routing/routing.module";
   ],
   imports: [
     BrowserModule,
-    RoutingModule
+    BrowserAnimationsModule,
+    RoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FlexLayoutModule
   ],
   providers: [FruitsService],
   bootstrap: [AppComponent]
--- steps.md
+++ steps.md
@@ -15,3 +15,14 @@ fruitsservices
 prop átadások
 stílusok
 angular komponensek
+
+
+https://medium.com/@ladyleet/popups-modals-and-navigation-using-angular-material-2-components-in-your-angular-2-project-faf510dbcdee
+https://medium.com/@ladyleet/setting-up-your-first-angular-2-project-using-angular-material2-5db18a174165
+https://dev-plaza.com/responsive-navbar-mit-angular-flex-layout-und-angular-material
+https://stackoverflow.com/questions/38209713/how-to-make-a-responsive-nav-bar-using-angular-material-2
+[ng-bootstrap]: https://codingthesmartway.com/using-bootstrap-with-angular/
+https://github.com/angular/angular-cli/wiki/stories-include-bootstrap
+ngx-bootstrap
+https://loiane.com/2017/08/how-to-add-bootstrap-to-an-angular-cli-project/#4-bootstrap-javascript-components-with-ngx-bootstrap-option-1
+
