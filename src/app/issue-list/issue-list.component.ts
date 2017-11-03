c8e81d6f7ba6ae96cb346d1334c167ec6ac06b37
Győző Horváth <gyozo.horvath@inf.elte.hu>
2017-11-03 16:20:34 +0100

    issue-list: filter works

--- src/app/issue-list/issue-list.component.ts
+++ src/app/issue-list/issue-list.component.ts
@@ -1,19 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Issue } from "../issue";
+
+const issues: Issue[] = [
+  {id: 1, location: 'PC5', description: 'Something wrong 1', status: 'ADDED'},
+  {id: 2, location: 'PC3', description: 'Something wrong 2', status: 'ASSIGNED'},
+  {id: 3, location: 'PC4', description: 'Something wrong 3', status: 'ADDED'},
+  {id: 4, location: 'PC2', description: 'Something wrong 4', status: 'DONE'},
+];
 
 @Component({
   selector: 'app-issue-list',
   templateUrl: './issue-list.component.html',
   styleUrls: ['./issue-list.component.css']
 })
-export class IssueListComponent {
+export class IssueListComponent implements OnInit {
 
-  issues: Issue[] = [
-    {id: 1, location: 'PC5', description: 'Something wrong 1', status: 'ADDED'},
-    {id: 2, location: 'PC3', description: 'Something wrong 2', status: 'ASSIGNED'},
-    {id: 3, location: 'PC4', description: 'Something wrong 3', status: 'ADDED'},
-    {id: 4, location: 'PC2', description: 'Something wrong 4', status: 'DONE'},
-  ]
+  issues;
 
   statuses: string[] = [
     'ADDED', 'ASSIGNED', 'DONE'
@@ -21,8 +23,17 @@ export class IssueListComponent {
 
   selectedStatus = ''
 
+  ngOnInit() {
+    this.filterIssues();
+  }
+
   onFilterChange(value) {
     this.selectedStatus = value;
+    this.filterIssues();
+  }
+
+  filterIssues() {
+    this.issues = this.selectedStatus === '' ? issues : issues.filter(issue => issue.status === this.selectedStatus);
   }
 
 }
